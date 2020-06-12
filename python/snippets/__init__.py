df9572ebbce290463fe9e7c0c8dedd482d3841e0
tutunak <dk@tutunak.com>
2020-06-12 15:37:29 +0300

    added init parse with exceptions

--- python/snippets/__init__.py
+++ python/snippets/__init__.py
@@ -1,4 +1,5 @@
 import loggin
+import ConfigParser
 
 
 def simple_logger():
@@ -34,3 +35,23 @@ def simple_logger():
     logger = logging.getLogger(__name__)
 
     return logger
+
+def ini_parse_exceptions():
+    """
+    Parsing INI and exceptions
+    :return: opt
+    """
+    try:
+        config = ConfigParser.ConfigParser()
+        config.readfp(io.BytesIO(ini_file))
+        out = config.get(section, option)
+    except ConfigParser.NoSectionError:
+        logging.error('No section {0} in file'.format(section))
+    except ConfigParser.MissingSectionHeaderError:
+        logging.error('No valid ini file')
+    except ConfigParser.NoOptionError:
+        logging.error('Option "{0}" not found in section {1}'.format(option, section))
+    except Exception as e:
+        logging.error('Unexpected error {0} {1} '.format(e.message, e.args))
+
+    return out
